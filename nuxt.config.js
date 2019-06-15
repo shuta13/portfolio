14fd0fcc00f6e534b8e476d4d40a50c81b8ee109
shuta13 <hollystarsun@gmail.com>
2019-06-16 02:01:58 +0900

    fixed description and 404 page letters a little

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,12 +10,10 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'did0es portfolio' },
       { hid: 'og:site_name', property: 'og:site_name', content: 'サイト名' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
       { hid: 'og:url', property: 'og:url', content: 'https://did0es.me' },
       { hid: 'og:title', property: 'og:title', content: 'did0es portfolio' },
-      { hid: 'og:description', property: 'og:description', content: 'did0es portfolio' },
       { hid: 'og:image', property: 'og:image', content: 'https://did0es.me/thumbnail-404.png' }
     ],
     link: [
