4f0f56b89fbefadcd51abc2adb0afb7db45fe712
shuta13 <hollystarsun@gmail.com>
2019-06-17 06:34:24 +0900

    added top page

---
+++ pages/garelly/index.vue
@@ -0,0 +1,62 @@
+<template lang="pug">
+  .container
+    .main
+      span.title garelly
+    .menu-wrap
+      input.toggle(type="checkbox")
+      .hamburger
+      .menu
+        ul.page-items
+          li top
+          li hoge
+          li foo
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  /* margin: 0 auto; */
+  min-height: 100vh;
+  display: flex;
+  /* justify-content: center;
+  align-items: center;
+  text-align: center; */
+  font-size: 62.5%;
+  background-color: #ccc;
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.main {
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  background-color: red;
+  position: absolute;
+  color: #fff;
+  font-family: 'CutiveMono';
+  font-size: 4vw;
+}
+
+.menu-wrap {
+  position: absolute;
+  .page-items {
+    background-color: aqua;
+    color: #fff;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,16 @@
 <template lang="pug">
   .container
-    h1 top
+    .main
+      span.title did0es portfolio
+      .item-wrap
+        a(href="https://twitter.com/did0es")
+          img.item(src="~/assets/images/twitter-square-brands.svg")
+        a(href="https://github.com/shuta13")
+          img.item(src="~/assets/images/github-square-brands.svg")
+        a(href="https://www.facebook.com/profile.php?id=100028982675881")
+          img.item(src="~/assets/images/facebook-square-brands.svg")
+        nuxt-link.link-wrap(to="/")
+          span.link GARELLY
 </template>
 
 <script>
@@ -9,14 +19,79 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+.container {
+  font-size: 62.5%;
+}
+
+a {
+  text-decoration: none;
+}
+</style>
+
+<style lang="scss" scoped>
 .container {
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
+  align-items: center;
+  text-align: center;
+  background: url('~assets/images/bg.png') no-repeat 50% 50%
+}
+
+.main {
+  // background-color: #777;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
   justify-content: center;
   align-items: center;
   text-align: center;
-  font-size: 62.5%;
+  width: 56vw;
 }
+
+.title {
+  // background-color: aquamarine;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  color: #fff;
+  font-family: 'CutiveMono';
+  font-size: 4vw;
+  height: 8vh;
+  width: 56vw;
+  margin-bottom: 2vh;
+}
+
+.item-wrap {
+  $item-margin: 3vh;
+  // background-color: #ccc;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+
+  .item {
+    // background-color: #000;
+    margin-left: $item-margin;
+    margin-right: $item-margin;
+    width: 4vw;
+    height: 8vh;
+  }
+
+  .link-wrap {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .link {
+    // background-color: red;
+    margin-left: $item-margin;
+    margin-right: $item-margin;
+    font-family: 'CutiveMono';
+    color: #fff;
+    font-size: 2vw;
+  }
+}
+
 </style>
