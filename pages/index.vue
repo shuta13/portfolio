fb8195c295b29076d86b8d9aa76207b9439a946e
shuta13 <hollystarsun@gmail.com>
2019-06-17 12:54:31 +0900

    fixed 404 resposinve

--- pages/index.vue
+++ pages/index.vue
@@ -38,7 +38,7 @@ a {
   text-align: center;
   background: url('~assets/images/bg.png') no-repeat 50% 50%;
   @media screen and (max-width: 767px) {
-    width: 100vw;
+    width: auto;
     height: 100vh;
   }
 }
