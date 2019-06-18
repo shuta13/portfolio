fde15b4fae5506b7ab8e66752248672370e158fe
shuta13 <hollystarsun@gmail.com>
2019-06-19 01:19:22 +0900

    added toggle menu

--- components/ToggleMenu/index.vue
+++ components/ToggleMenu/index.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
   .container
     .menu-wrap
-      input.toggle(type="checkbox")
+      //- input.toggle(type="checkbox")
       .hamburger
+        .line
       .menu
         ul.page-items
           li top
@@ -20,7 +21,6 @@ export default {
 
 <style lang="scss" scoped>
 .container {
-  position: fixed; // sticky menuの実装
   // margin: 0 auto;
   // min-height: 100vh; こいつonにするとページ全体に範囲が及ぶ
   font-size: 62.5%;
@@ -34,17 +34,19 @@ ul {
 }
 
 .title {
-  background-color: red;
   position: absolute;
   width: 100vw;
   height: 64px;
 }
 
 .menu-wrap {
+  position: fixed; // sticky menuの実装
+  z-index: 1;
   .toggle {
     position: absolute;
     top: 0;
     left: 0;
+    z-index: 2;
     cursor: pointer;
     width: 50px;
     height: 50px;
@@ -54,6 +56,36 @@ ul {
     position: absolute;
     top: 0;
     left: 0;
+    z-index: 1;
+    width: 60px;
+    height: 60px;
+    padding: 1rem;
+    background: rgb(255, 255, 255);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .line {
+      position: relative;
+      width: 100%;
+      height: 2px;
+      background-color: rgb(0, 0, 0);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .line:before,
+    .line:after {
+      content: '';
+      position: absolute;
+      z-index: 1;
+      top: -10px;
+      width: 100%;
+      height: 2px;
+      background-color: inherit
+    }
+    .line:after {
+      top: 10px;
+    }
   }
 }
 </style>
