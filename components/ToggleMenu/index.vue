c3715476ecc2d3b2a177eb66ef20cee720b55113
shuta13 <hollystarsun@gmail.com>
2019-06-18 19:57:50 +0900

    added responsive images

---
+++ components/ToggleMenu/index.vue
@@ -0,0 +1,58 @@
+<template lang="pug">
+  .container
+    .menu-wrap
+      input.toggle(type="checkbox")
+      .hamburger
+      .menu
+        ul.page-items
+          li top
+          li graphics
+          li generative
+          li other
+          li about
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  // margin: 0 auto;
+  // min-height: 100vh; こいつonにするとページ全体に範囲が及ぶ
+  font-size: 62.5%;
+  // background-color: #000;
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.main {
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  background-color: red;
+  position: absolute;
+  color: #fff;
+  font-family: 'CutiveMono';
+  font-size: 4vw;
+}
+
+.menu-wrap {
+  position: absolute;
+  .page-items {
+    background-color: aqua;
+    color: #fff;
+  }
+}
+</style>
--- pages/garelly/index.vue
+++ pages/garelly/index.vue
@@ -1,62 +1,63 @@
 <template lang="pug">
   .container
-    .main
-      span.title garelly
-    .menu-wrap
-      input.toggle(type="checkbox")
-      .hamburger
-      .menu
-        ul.page-items
-          li top
-          li hoge
-          li foo
+    toggle-menu
+    .whole-wrap
+      .image-wrap
+        .image(:style="{ 'background-image': 'url(/top/work01.png)' }")
+        .image(:style="{ 'background-image': 'url(/top/work02.png)' }")
+        .image(:style="{ 'background-image': 'url(/top/work03.png)' }")
+        .image(:style="{ 'background-image': 'url(/top/work03.png)' }")
+        .image(:style="{ 'background-image': 'url(/top/work01.png)' }")
+        .image(:style="{ 'background-image': 'url(/top/work02.png)' }")
 </template>
 
 <script>
-export default {
+import ToggleMenu from '~/components/ToggleMenu'
 
+export default {
+  components: {
+    ToggleMenu
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .container {
-  /* margin: 0 auto; */
-  min-height: 100vh;
-  display: flex;
-  /* justify-content: center;
-  align-items: center;
-  text-align: center; */
-  font-size: 62.5%;
-  background-color: #ccc;
-}
-
-ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
-
-.main {
   margin: 0 auto;
+}
+
+.whole-wrap {
   display: flex;
   justify-content: center;
   align-items: center;
-  text-align: center;
 }
 
-.title {
-  background-color: red;
-  position: absolute;
-  color: #fff;
-  font-family: 'CutiveMono';
-  font-size: 4vw;
+.image-wrap {
+  margin-top: 8vh;
+  width: 96vw;
+  // height: 96vh;
+  display: flex;
+  justify-content: center;
+  // align-items: center; // 消すと一番上から始まる
+  flex-wrap: wrap;
+  background-color: blue;
+  // widthを調整(@mediaで)
+  // 見切れた部分はロールして見れるように
+  // flex-wrapでよさそう
+  // 眠い
 }
 
-.menu-wrap {
-  position: absolute;
-  .page-items {
-    background-color: aqua;
-    color: #fff;
+.image {
+  background-position: center center;
+  // min-width: 360px;
+  // min-height: 240px;
+  width: 32vw;
+  height: 24vw;
+  @media screen and (max-width: 768px) {
+    width: 96vw;
+    height: 72vw;
   }
+  object-fit: cover;
+  overflow: hidden;
 }
 </style>
