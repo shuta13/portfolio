8a6635de864cc802a867d51f5dec11d7f15489d7
shuta13 <hollystarsun@gmail.com>
2019-07-05 17:26:57 +0900

    made test endpoint

---
+++ pages/hoge.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  deep-sea
+</template>
+
+<script>
+import DeepSea from '~/components/garelly/works/DeepSea'
+
+export default {
+  components: {
+    DeepSea
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
